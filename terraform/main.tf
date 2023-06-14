ea59c917f935df3b1abecef095abfaa6c0f9fdd3
aj <50439018+arjhonlajom@users.noreply.github.com>
2023-06-14 14:04:02 +0800

    updates after reset head

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,44 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "3.8.0"
     }
   }
 
   required_version = ">= 1.1.0"
 }
 
+variable "subscription_id" {
+ type = string
+ default = "0000-0000-0000-0000"
+ description = "Azure Subscription ID"
+}
+
+variable "tenant_id" {
+ type = string
+ default = "0000-0000-0000-0000"
+ description = "Tenant ID"
+}
+
+variable "client_id" {
+ type = string
+ default = "0000-0000-0000-0000"
+ description = "Client ID"
+}
+
+variable "client_secret" {
+ type = string
+ default = "0000-0000-0000-0000"
+ description = "Client Secret"
+}
+
+
 provider "azurerm" {
+  subscription_id   =  var.subscription_id  
+  tenant_id         =  var.tenant_id        
+  client_id         =  var.client_id        
+  client_secret     =  var.client_secret    
+  
   features {}
 }
 
